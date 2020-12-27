633ac51937a8534af91118f62b698c3e4782e9f9
jonathanvigren <53133767+jonathanvigren@users.noreply.github.com>
2020-12-27 10:08:18 +0100

    Add files via upload

--- todo list/script.js
+++ todo list/script.js
@@ -1,28 +1,47 @@
 let listItems = [];
 var i = 0;
 
+
+
 function getValue() {
   const inputText = document.querySelector(".textinput").value;
   listItems.push(inputText);
 }
 
 function addListElement() {
-    var listItem = document.createElement("div");
-    listItem.className = "item";
-    listItem.id = i;
-    listItem.innerHTML = listItems[i];
-    var element = document.getElementById("taskcontainer");
-    element.appendChild(listItem);
-    i++;
-    var trash = document.createElement("div");
-    trash.className = "trash";
-    trash.innerHTML = "🗑️";
-    trash.onclick = function trashBin () {
-      this.parentElement.remove();
-    };
-    listItem.appendChild(trash);
-  }
+  localStorage.setItem('listItemsRef', JSON.stringify(listItems));
+  var listItem = document.createElement("div");
+  listItem.className = "item";
+  listItem.id = i;
+  listItem.innerHTML = listItems[i];
+  var element = document.getElementById("taskcontainer");
+  element.appendChild(listItem);
+  i++;
 
-function trashBin () {
+  var trash = document.createElement("div");
+  trash.className = "trash";
+  trash.innerHTML = "🗑️";
+  trash.onclick = function trashBin() {
+    this.parentElement.remove();
+  };
+  listItem.appendChild(trash);
+
+  var toggle = document.createElement("INPUT");
+  toggle.className = "checkbox";
+  toggle.setAttribute("type", "checkbox");
+  listItem.appendChild(toggle);
+}
+
+function trashBin() {
   this.parentElement.remove();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('listItemsRef');
+  if (ref) {
+    listItems = JSON.parse(ref);
+    listItems.forEach(t => {
+      addListElement(t);
+    });
+  }
+});
