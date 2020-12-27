6e58047fbc92f013562fb82b5de72ee549e3f7ff
jonathanvigren <53133767+jonathanvigren@users.noreply.github.com>
2020-12-27 07:59:28 +0100

    Add files via upload

--- todo list/script.js
+++ todo list/script.js
@@ -1,1 +1,28 @@
-let todoListItems = [];
+let listItems = [];
+var i = 0;
+
+function getValue() {
+  const inputText = document.querySelector(".textinput").value;
+  listItems.push(inputText);
+}
+
+function addListElement() {
+    var listItem = document.createElement("div");
+    listItem.className = "item";
+    listItem.id = i;
+    listItem.innerHTML = listItems[i];
+    var element = document.getElementById("taskcontainer");
+    element.appendChild(listItem);
+    i++;
+    var trash = document.createElement("div");
+    trash.className = "trash";
+    trash.innerHTML = "🗑️";
+    trash.onclick = function trashBin () {
+      this.parentElement.remove();
+    };
+    listItem.appendChild(trash);
+  }
+
+function trashBin () {
+  this.parentElement.remove();
+}
